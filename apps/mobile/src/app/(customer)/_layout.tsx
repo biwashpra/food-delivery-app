d48cc4ca261c956c33a5d26c17a3c8c4b1d4df4d
Biwash Pradhan <biwashpra3883@gmail.com>
2026-08-06 10:00:49 +0545

    feat: seach and zustand implementation

--- apps/mobile/src/app/(customer)/_layout.tsx
+++ apps/mobile/src/app/(customer)/_layout.tsx
@@ -3,7 +3,7 @@ import { NativeTabs } from "expo-router/unstable-native-tabs";
 export default function CustomerLayout() {
   return (
     <NativeTabs>
-      <NativeTabs.Trigger name="index">
+      <NativeTabs.Trigger name="(home)">
         <NativeTabs.Trigger.Label>Home</NativeTabs.Trigger.Label>
         <NativeTabs.Trigger.Icon sf="house.fill" md="home" />
       </NativeTabs.Trigger>
